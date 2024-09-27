1101713bed5e9d39bb2b54fbc68c3cbb8f1d4a8e
B Shrinidhi <b.shrinidhi1214@gmail.com>
2024-09-27 17:10:19 +0530

    one image adding functionality working - uuid remaining

--- db/schema/userSchema.js
+++
@@ -1,9 +0,0 @@
-// import { serial, text } from "drizzle-orm/mysql-core";
-import { pgTable,text,serial } from "drizzle-orm/pg-core";
-
-export const users=pgTable("users",{
-    id:serial("id").primaryKey(),
-    email:text("email").notNull(),
-    password:text("password").notNull(),
-    role:text("role").notNull()
-})
