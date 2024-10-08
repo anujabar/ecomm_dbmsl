1571c83371aed8df1d0b268ae09fdcc68ae4418a
Anuja Barawkar <anuja.barawkar@gmail.com>
2024-10-08 18:31:24 +0530

    Search functionality

--- app/products/all/page.jsx
+++ app/products/all/page.jsx
@@ -8,9 +8,37 @@ import ProductCard from '@/app/(components)/ProductCard';
 
 
 const ProductList = () => {
+  const ratings=[5,4,3,2,1]
+  const [categories,setCategories]=useState([])
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [isFilter,setIsFilter]=useState(false)
+  const [filter,setFilter]=useState("")
   const [errorMessage, setErrorMessage] = useState(null);
   const {user}=useAuthContext()
+  const handleCat=(e)=>{
+    const fil=[]
+    products.forEach((p)=>{
+      if(p.category===e.target.value){
+        fil.push(p)
+      }
+    })
+    setFilteredProducts(fil)
+  }
+  const handleRating=(e)=>{
+    const fil=[]
+    products.forEach((p)=>{
+      const n=Math.round(p.stars/p.reviews)
+      if(n==e.target.value){
+        console.log(Math.round(p.stars/p.reviews))
+        fil.push(p)
+      }
+    })
+    console.log(e.target.value)
+    console.log("FIL ",fil)
+    setFilteredProducts(fil)
+  }
+  
   const handleCart=async (id)=>{
     try{
       const response=await fetch('/api/cart',{
@@ -39,7 +67,15 @@ const ProductList = () => {
           throw new Error('Failed to fetch products');
         }
         const data = await response.json();
+        let c=[]
+        data.products.forEach((p)=>{
+          if(!c.includes(p.category)){
+            c.push(p.category)
+          }
+        })
+        setCategories(c)
         setProducts(data.products); 
+        setFilteredProducts(data.products); 
       } catch (error) {
         setErrorMessage(error.message);
       }
@@ -52,8 +88,35 @@ const ProductList = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Products</h1>
+      <div>
+        <h3>Filter by</h3>
+        {
+          <select onClick={(e)=>{setFilter(e.target.value)}}>
+            <option value="All" >All</option>
+            <option value="Category">Category</option>
+            <option value="Rating">Rating</option>
+          </select>
+        }
+        {
+          filter!=="" && filter=='Category' && <select onClick={handleCat}>
+            {categories.map((c)=>{
+              return <option key={c} value={c}>{c}</option>
+            })}
+          </select>
+        }
+        {
+          filter!=="" && filter=='Rating' && <select onClick={handleRating}>
+            {ratings.map((c)=>{
+              return <option key={c} value={c}>{c}</option>
+            })}
+          </select>
+        }
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map((product, ind) => {
+        {(filter!=="" && filter!=="All") && filteredProducts.map((product, ind) => {
+          return <ProductCard product={product} key={ind}/>
+        })}
+        {(filter==="" || filter==="All") && products.map((product, ind) => {
           return <ProductCard product={product} key={ind}/>
         })}
       </div>
